10162887921438455423c33428be0a9e9f38da55
Jeremy Ellis <keyfreemusic@gmail.com>
2025-11-05 23:50:32 -0800

    Create blink-monitor-serial.ino

---
+++ arduinoIDEv2/blink-monitor-serial.ino
@@ -0,0 +1,40 @@
+/*
+  DigitalReadSerial
+
+  Reads a digital input on pin 2, prints the result to the Serial Monitor
+
+  This example code is in the public domain.
+
+  https://docs.arduino.cc/built-in-examples/basics/DigitalReadSerial/
+*/
+#include <Arduino_RouterBridge.h>
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
+  Monitor.begin();
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button:
+  Serial.println(buttonState);
+  if (buttonState == LOW){
+  digitalWrite(LED_BUILTIN, LOW);
+
+  } else {
+     digitalWrite(LED_BUILTIN, HIGH);
+     Monitor.println("Cool");
+  }
+  delay(1);  // delay in between reads for stability
+}
